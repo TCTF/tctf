1f8354b765a261b641d0b208e5f569e1b159356d
destinykids <31513506+destinykids@users.noreply.github.com>
2017-09-21 11:56:00 -0400

    Now provides three different streams and exception handling to a file.

--- failsafe.bot/bot.js
+++ failsafe.bot/bot.js
@@ -2,41 +2,72 @@
  var logger = require('winston');
  var auth = require('./auth.json');
 
- // Configure logger settings
- logger.remove(logger.transports.Console);
- logger.add(logger.transports.Console, {
- 	colorize: true
+ //setup logging.
+
+ var logger = new(winston.Logger)({
+   transports: [
+     new(winston.transports.File)({
+       name: 'info-file',
+       filename: 'failsafe-info.log',
+       level: 'info'
+     }),
+     new(winston.transports.File)({
+       name: 'debug-file',
+       filename: 'failsafe-debug.log',
+       level: 'debug'
+     }),
+     new(winston.transports.File)({
+       name: 'error-file',
+       filename: 'failsafe-error.log',
+       level: 'error'
+     }),
+     new(winston.transports.Console)({
+       colorize: true,
+       level: 'debug'
+     })
+   ],
+   exceptionHandlers: [
+     new(winston.transports.File)({
+       name: 'exceptions-file',
+       filename: 'failsafe-exceptions.log',
+       handleExceptions: true,
+       humanReadableUnhandledException: true
+     })
+   ],
+   level = 'info'
  });
 
-logger.level = 'debug';
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-	token: auth.token,
-	autorun: true
-});
-bot.on('ready', function(evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-	// Our bot needs to know if it will execute a command
-	// It will listen for messages that start with '!'
-	if (message.substring(0,1) == '!') {
-		var args = message.substring(1).split(' ');
-		var cmd = args[0];
+ // Initialize Discord Bot
+ var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+ });
+ bot.on('ready', function(evt) {
+   logger.info('Connected');
+   logger.info('Logged in as: ');
+   logger.debug(bot.username + ' - (' + bot.id + ')');
+   logger.debug('Using token ID' + auth.token);
+ });
+ bot.on('message', function(user, userID, channelID, message, evt) {
+       // Our bot needs to know if it will execute a command
+       // It will listen for messages that start with '!'
+       if (message.substring(0, 1) == '!') {
+         logger.debug('Command detected');
+         var args = message.substring(1).split(' ');
+         var cmd = args[0];
+         logger.debug('Command used: '+ cmd); 
 
-		args = args.splice(1);
-		switch(cmd) {
-			// !ping
-			case 'ping':
-				bot.sendMessage({
-					to: channelID,
-					message: 'Pong!'
-				});
-			break;
-			// Just add any case commands if you want to..
-		}
-	}
-}
+         args = args.splice(1);
+         switch (cmd) {
+           // !ping
+           case 'ping':
+             bot.sendMessage({
+               to: channelID,
+               message: 'Pong!'
+             });
+             break;
+             // Just add any case commands if you want to..
+         }
+       }
+     }
